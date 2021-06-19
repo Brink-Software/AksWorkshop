eadfc3f0bd4ec742e4b6bf1f938c6bc7abc375ba
Wema <w.wuyts@brink.nl>
2021-06-19 15:23:43 +0200

    lab5 prep

---
+++ exercises/lab5/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ "$APP_COLOR" == "Blue" ];
+then
+    sed -i "s/APP_COLOR/$APP_COLOR/g" /usr/share/nginx/html/index.html
+elif [ "$APP_COLOR" == "Red" ];
+then
+    sed -i "s/APP_COLOR/$APP_COLOR/g" /usr/share/nginx/html/index.html
+elif [ "$APP_COLOR" == "Purple" ];
+then
+    sed -i "s/APP_COLOR/$APP_COLOR/g" /usr/share/nginx/html/index.html    
+elif [ "$APP_COLOR" == "Orange" ];
+then
+    sed -i "s/APP_COLOR/$APP_COLOR/g" /usr/share/nginx/html/index.html        
+else
+    sed -i "s/APP_COLOR/Green/g" /usr/share/nginx/html/index.html              
+fi  
+
+if test -z "$APP_MESSAGE"; then
+      sed -i "s/APP_MESSAGE/Hello There!/g" /usr/share/nginx/html/index.html
+else
+      sed -i "s/APP_MESSAGE/$APP_MESSAGE/g" /usr/share/nginx/html/index.html
+fi
